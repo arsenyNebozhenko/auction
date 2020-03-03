1f42aa3e638d4790ad783892eacce50b95a69710
arsenyNebozhenko <nebozhenko.arseny@gmail.com>
2020-03-03 17:24:06 +0300

    refactoring and added addPrice

--- src/components/Lot/Lot.js
+++ src/components/Lot/Lot.js
@@ -6,6 +6,11 @@ const Lot = ({ id, title, price, add, updateItemProp }) => {
     updateItemProp(id, name, value)
   }
 
+  const addPrice = () => {
+    updateItemProp(id, 'price', +price + +add)
+    updateItemProp(id, 'add', '')
+  }
+
   return (
     <li className="lot">
       <input type="text" className="lot__input lot__input--size-lg" value={title} name="title" onChange={handleChange} />
@@ -13,7 +18,7 @@ const Lot = ({ id, title, price, add, updateItemProp }) => {
       <input type="text" className="lot__input" value={price} name="price" onChange={handleChange} />
       <div className="lot__sign">₽</div>
       <input type="text" className="lot__input lot__input--size-sm" value={add} name="add" onChange={handleChange} />
-      <button className="lot__button">+</button>
+      <button className="lot__button" onClick={addPrice}>+</button>
     </li>
   )
 }
--- src/components/Lots/Lots.js
+++ src/components/Lots/Lots.js
@@ -7,7 +7,16 @@ import { updateItemProp } from '../../actions/lots'
 const Lots = ({ lots, updateItemProp }) => {
   return (
     <ul className="lots">
-      {lots.map(lot => <Lot key={lot.id} id={lot.id} title={lot.title} price={lot.price} add={lot.add} updateItemProp={updateItemProp} />)}
+      {lots.map(lot => 
+        <Lot 
+          key={lot.id} 
+          id={lot.id} 
+          title={lot.title} 
+          price={lot.price} 
+          add={lot.add} 
+          updateItemProp={updateItemProp} 
+        />
+      )}
     </ul>
   )
 }
